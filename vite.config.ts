dbb22a61e810f89f6453efbdfd0d28d0d91d4cc3
linoxbt <alemzdelight@gmail.com>
2026-04-30 13:22:38 +0100

    Update vite.config.ts with TanStack Router plugin and include generated route tree

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,12 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    TanStackRouterVite(),
     react(),
     tailwindcss(),
     tsconfigPaths(),
